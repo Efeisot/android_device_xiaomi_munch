36250be170752ad825d094c7aaf1bd9da02ac554
Efeisot <104940108+Efeisot@users.noreply.github.com>
2025-02-13 23:42:05 +0300

    Update BoardConfig.mk
    It looks like I'm really fool

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,9 +19,6 @@ TARGET_SCREEN_DENSITY := 440
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_munch
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_munch
 
-# Kernel
-TARGET_KERNEL_CONFIG += vendor/xiaomi/munch.config
-
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := munch,munch_global,munch_in
 
