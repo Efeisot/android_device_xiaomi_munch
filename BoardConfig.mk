8bcc9836969b00bd8fef9b8766a2ac96eb077044
Rik <aritrarik536@protonmail.com>
2025-04-03 13:13:22 +0530

    Revert "munch: Switch to defconfig for kernel compilation"
    This reverts commit 5e7d89185978f05a01b71da8a36693b751721bb2.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,6 +22,12 @@ TARGET_SCREEN_DENSITY := 440
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_munch
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_munch
 
+# Kernel
+TARGET_KERNEL_CONFIG += vendor/xiaomi/munch.config
+
+# Leica Camera
+TARGET_INCLUDES_MIUI_CAMERA := true
+
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := munch,munch_global,munch_in
 
